4dad43b23254f4e3a0e0e2b9afb1a577fdb30648
gfizzle <winstongdouglas4@gmail.com>
2026-01-18 19:42:37 -0600

    first commit, section 4 video #8 mapping Integers in Spark RDD what is currently in Main.java is a reduce function on some integers

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         List<Integer> inputData = new ArrayList<>();
         inputData.add(35);
