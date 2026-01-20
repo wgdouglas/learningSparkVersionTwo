8773631dfe624ada49efb980fbecd764a9c206a9
gfizzle <winstongdouglas4@gmail.com>
2026-01-19 18:20:55 -0600

    Chapter 13 Tuples & RDD's

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +28,12 @@ public class Main {
         JavaRDD<Integer> originalIntegersRdd = spackContext.parallelize(inputData);
 
         // This is used to map values into a functions desired value
-        //You'll use the java approach which should not be used consistently in Spark Java Development
-        JavaRDD<IntegerWithSquareRoot> squareRootRDD = originalIntegersRdd.map( value -> new IntegerWithSquareRoot(value));
+        //This is the view of Tuples in Java/Scala with Spark Java Development which is extensively used
+        JavaRDD<Tuple2<Integer, Double>> squareRootRDD = originalIntegersRdd.map( value -> new Tuple2<>(value, Math.sqrt(value)) );
+
+        // This is where you'll use Tuples with the Object `Tuple2<>` which allows you to
+        // bypass Java Object class creation for grouping values like in a list
+        Tuple2<Integer, Double> groupedValueTuple = new Tuple2<> ( 9 , 3.0);// the 3.0 is a Double b/c it's a square root
 
         spackContext.close();
 
